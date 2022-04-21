9b80ff8c6605cf2908eee8f5ef45802a669cada1
kaayran <dodororo132@gmail.com>
2022-04-21 04:10:49 +0300

    create sign-up form, add flash msgs to base template

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -13,6 +13,18 @@ def logout():
     return render_template('logout.html')
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        repeat_password = request.form.get('repeatPassword')
+
+        if password != repeat_password:
+            flash('Passwords not the same.', category='error')
+            pass
+        else:
+            flash('Successful registration of a new user.', category='success')
+
     return render_template('sign_up.html')
